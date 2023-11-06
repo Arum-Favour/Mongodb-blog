33aebd08463e66411e403831a7479a2ef00b5c6c
Kingson.Arum <nwajiudo@gmail.com>
2023-11-06 23:22:32 +0100

    commit

---
+++ app.js
@@ -0,0 +1,95 @@
+//jshint esversion:6
+import mongoose from "mongoose";
+import express from "express";
+import bodyParser from "body-parser";
+import _ from "lodash";
+//const ejs = require("ejs");
+
+mongoose.connect(
+  "mongodb+srv://Kingson:KavMn5YvwC6STQmT@cluster0.al8mjom.mongodb.net/postDB"
+);
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+const Post = mongoose.model("Post", postSchema);
+
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//let posts = [];
+await Post.findByIdAndRemove({ _id: "652924d2822f04d70d556bb3" });
+app.get("/", async function (req, res) {
+  const defaultPost = new Post({
+    title: "Home",
+    content:
+      "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.",
+  });
+  //defaultPost.save();
+  const foundPosts = await Post.find({});
+  res.render("home", {
+    startingContent: defaultPost,
+    posts: foundPosts,
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  const newpost = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  });
+  newpost.save();
+  //posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:postName", async function (req, res) {
+  const requestedTitle = req.params.postName; 
+  const resp = await Post.findOne({ title: requestedTitle }).exec();
+  if (resp) {
+    res.render("post", {
+      title: resp.title,
+      content: resp.content,
+    });
+  }else{
+    res.status(404 ,error("Data not Found"));
+  }
+  // Post.forEach(function (post) {
+  //   const storedTitle = _.lowerCase(post.title);
+
+  // //   if (storedTitle === requestedTitle) {
+  // //     res.render("post", {
+  // //       title: post.title,
+  // //       content: post.content,
+  // //     });
+  // //   }
+  // // });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
